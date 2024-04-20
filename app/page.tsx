015d659d1286daa744711ae17a8348686a5148ad
Gopal Sasmal <gopalsasmal2000@gmail.com>
2024-04-20 10:35:24 +0530

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Encryption from "@/components/main/Encryption";
+import Hero from "@/components/main/Hero";
+import Projects from "@/components/main/Projects";
+import Skills from "@/components/main/Skills";
+
+export default function Home() {
+  return (
+    <main className="h-full w-full">
+      <div className="flex flex-col gap-20">
+        <Hero />
+        {/* <Skills />
+        <Encryption />
+        <Projects /> */}
+      </div>
+    </main>
+  );
+}
---
+++ constants/index.ts
@@ -0,0 +1,262 @@
+import { FaLinkedinIn } from "react-icons/fa";
+
+export const Skill_data = [
+  {
+    skill_name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Java Script",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    Image: "/redux.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React Query",
+    Image: "/reactquery.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Type Script",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next js 13",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Framer Motion",
+    Image: "/framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Stripe Payment",
+    Image: "/stripe.webp",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Mongo db",
+    Image: "/mongodb.png",
+    width: 40,
+    height: 40,
+  },
+];
+
+interface SocialsType {
+  name: string;
+  profile_link: string;
+  icon: string;
+}
+
+export const Socials: SocialsType[] = [
+  {
+    name: "Github",
+    profile_link: "https://github.com/gopalsasmal100xdev",
+    icon: "/icons8-github.svg",
+  },
+  {
+    name: "LinkedIn",
+    profile_link: "https://www.linkedin.com/in/gopal-sasmal/",
+    icon: "/icons8-linkedin.svg",
+  },
+  {
+    name: "Twitter",
+    profile_link: "https://twitter.com/GopalSasmal01",
+    icon: "/icons8-twitter.svg",
+  },
+];
+
+export const Frontend_skill = [
+  {
+    skill_name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Java Script",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Material UI",
+    Image: "/mui.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    Image: "/redux.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React Query",
+    Image: "/reactquery.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Type Script",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next js 13",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+];
+
+export const Backend_skill = [
+  {
+    skill_name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Express js",
+    Image: "/express.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Mongo db",
+    Image: "/mongodb.png",
+    width: 40,
+    height: 40,
+  },
+  {
+    skill_name: "Fire base",
+    Image: "/Firebase.png",
+    width: 55,
+    height: 55,
+  },
+  {
+    skill_name: "Postger SQL",
+    Image: "/postger.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "My SQL",
+    Image: "/mysql.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Prisma",
+    Image: "/prisma.webp",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Graphql",
+    Image: "/graphql.png",
+    width: 80,
+    height: 80,
+  },
+];
+
+export const Full_stack = [
+  {
+    skill_name: "React Native",
+    Image: "/ReactNative .png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Tauri",
+    Image: "/tauri.svg",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Docker",
+    Image: "/docker.webp",
+    width: 70,
+    height: 70,
+  },
+
+  {
+    skill_name: "Figma",
+    Image: "/figma.png",
+    width: 50,
+    height: 50,
+  },
+];
+
+export const Other_skill = [
+  {
+    skill_name: "Go",
+    Image: "/go.png",
+    width: 60,
+    height: 60,
+  },
+];
+
+export const Info_me =
+  "I'm a skilled software developer with experience in JavaScript and TypeScript, and expertise in frameworks like React.js, Next.js, Node.js. I'm a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life!";
+
+export const Resume_url = "https://flowcv.com/resume/4upc6r21k2";
