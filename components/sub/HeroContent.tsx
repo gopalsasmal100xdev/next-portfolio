41c1cbfb67559a5dfd75e6d8a66abdeef98edf6d
Gopal Sasmal <gopalsasmal2000@gmail.com>
2024-04-21 16:13:43 +0530

    Feat: Contact section added

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -2,16 +2,15 @@
 
 import React from "react";
 import { motion } from "framer-motion";
-import {
-  slideInFromLeft,
-  slideInFromTop,
-} from "@/utils/motion";
+import { slideInFromLeft, slideInFromTop } from "@/utils/motion";
 import wavingHand from "@/public/waving-hand.gif";
 import { SparklesIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import { NAME, Resume_url, Short_desc, titles } from "@/constants";
 import Typewriter from "typewriter-effect";
 import Globe from "./Globe";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 const HeroContent = () => {
   return (
@@ -20,13 +19,6 @@ const HeroContent = () => {
       animate="visible"
       className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]">
       <div className="h-full w-full flex flex-col gap-5 justify-center mt-40 m-auto text-start">
-        <motion.div
-          variants={slideInFromTop}
-          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]">
-          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text text-[13px]">Fullstack Developer</h1>
-        </motion.div>
-
         <motion.div
           variants={slideInFromLeft(0.5)}
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto">
@@ -71,13 +63,12 @@ const HeroContent = () => {
           className="text-lg text-gray-400 my-5 max-w-[600px]">
           {Short_desc}
         </motion.p>
-        <motion.a
-          variants={slideInFromLeft(1)}
+        <a
           href={Resume_url}
           target="_blank"
           className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]">
           Resume
-        </motion.a>
+        </a>
       </div>
 
       <div className="w-full h-full lg:flex lg:justify-center lg:items-center hidden">
