4bfa026b4e9631400fa2204a1cdf54e11d671cef
GOPAL SASMAL <gopalsasmal2000@gmail.com>
2024-04-22 18:57:08 +0530

    Feat: Add contact section

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -6,13 +6,6 @@ const Hero = () => {
     <div
       className="relative flex flex-col h-full w-full -mt-[8rem]"
       id="about-me">
-      <video
-        autoPlay
-        muted
-        loop
-        className="rotate-180 absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover ">
-        <source src="/blackhole.webm" type="video/webm" />
-      </video>
       <HeroContent />
     </div>
   );
