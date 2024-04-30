24bc5c4ff0344ededc482aecbfc043a172a42f31
GOPAL SASMAL <gopalsasmal2000@gmail.com>
2024-04-30 19:06:51 +0530

    Feature: Take user Ip address

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -5,15 +5,15 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request, res: Response) {
-  const { fullname, email, message } = await req.json();
+  const { fullname, email, message, userIp } = await req.json();
   try {
     const data = await resend.emails.send({
       from: "ACK ME <onboarding@resend.dev>",
       to: ["gopalsasmal1806@gmail.com"],
       subject: "Portfolio Message",
-      react: EmailTemplate({ fullname, email, message }) as ReactElement,
+      react: EmailTemplate({ fullname, email, message, userIp}) as ReactElement,
     });
-    return Response.json(data);
+    return Response.json("data");
   } catch (error) {
     return Response.json({ error });
   }
